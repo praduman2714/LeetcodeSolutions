b2dcd0c4a64136e2e7ef494b158b91c258b489f1
praduman2714 <pradu14jsr@gmail.com>
2022-07-15 15:48:18 +0530

    LC#198

---
+++ HouseRobber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = dp[0] + nums[2];
+        for(int i = 3; i<nums.length; i++){
+            int val1 = 
+        }
+    }
+}
