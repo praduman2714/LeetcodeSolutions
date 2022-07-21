e0705cebdbc0aa36ff5fb3ae4e5fec11e1f0fdb4
praduman2714 <pradu14jsr@gmail.com>
2022-07-21 12:06:00 +0530

    Leetcode Daily Challange 21-07-2022

---
+++ ReverseLinkedList2.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null) return null;
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        while(left > 1){
+            prev = curr;
+            curr = curr.next;
+            left--;
+            right--;
+        }
+        ListNode conn = prev, tail = curr;
+        while(right > 0){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            right --;
+        }
+        if(conn != null)
+            conn.next = prev;
+        else
+            head = prev;
+        tail.next = curr;
+        return head;
+    }
+}
